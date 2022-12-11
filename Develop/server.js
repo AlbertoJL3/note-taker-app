bda08227849cd2e84a93a3a53a0d39349a618e63
Beto Lopez <363alberto@gmail.com>
2022-12-11 17:56:37 -0400

    added post request to handle notes posting

--- Develop/server.js
+++ Develop/server.js
@@ -25,6 +25,58 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
+app.post('/api/notes', (req, res) => {
+ 
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
+
+  // Destructuring assignment for the items in req.body
+  const { product, review, username } = req.body;
+
+  // If all the required properties are present
+  if (product && review && username) {
+    // Variable for the object we will save
+    const newReview = {
+      product,
+      review,
+      username,
+      review_id: uuid(),
+    };
+
+    // Obtain existing reviews
+    fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedReviews = JSON.parse(data);
+
+        // Add a new review
+        parsedReviews.push(newReview);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/reviews.json',
+          JSON.stringify(parsedReviews, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newReview,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
 
 
 
